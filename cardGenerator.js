ee0e4844ad9dcb1d1a617d6e545036d46a66e060
anthonyng288 <anthonynguyen832@gmail.com>
2022-10-29 21:38:01 +1000

    fix: fix spacing

--- cardGenerator.js
+++ cardGenerator.js
@@ -1,51 +1,51 @@
 
-constaxios=require('axios')
+const axios = require('axios')
 
-constsharp=require('sharp')
+const sharp = require('sharp')
 
-constimage='test3_original,'
-constcard='cards/2_of_clubs,'
+const image = 'test3_original, '
+const card = 'cards/2_of_clubs, '
 
-constsuits=["clubs","diamonds","hearts","spades","red","black"]
-constvalues=["ace","jack","queen","king","joker"]
+const suits = ["clubs", "diamonds", "hearts", "spades", "red", "black"]
+const values = ["ace", "jack", "queen", "king", "joker"]
 
-consturl="https://farm66.staticflickr.com/65535/52449834044_63d3b9393e_c,"
+const url = "https://farm66.staticflickr.com/65535/52449834044_63d3b9393e_c, "
 
 
 
-asyncfunctionstep1(){
-constimageResponse=awaitaxios({url:url,responseType:'arraybuffer'})
-constbuffer=Buffer.from(imageResponse.data,'binary')
+async function step1() {
+    const imageResponse = await axios({url: url, responseType: 'arraybuffer'})
+    const buffer = Buffer.from(imageResponse.data, 'binary') 
 
-sharp(buffer)
-.resize({
-width:308,
-height:607
-})
-.toBuffer({resolveWithObject:true})
-.then(({data,info})=>{
-sharp(card)
-.composite([
-{input:data}
-])
-.toFile("new_cards/uwu,")
-})
+    sharp(buffer)
+    .resize({
+        width: 308,
+        height: 607
+    })
+    .toBuffer({resolveWithObject: true})
+    .then(({ data, info }) =>{
+        sharp(card)
+        .composite([
+            {input: data} 
+        ])
+        .toFile("new_cards/uwu, ")
+    })
 }
 
-asyncfunctionstep2(buffer){
+async function step2(buffer) {
 
-
+    
 }
 
 
-asyncfunctionprocessImage(){
-step2(step1())
+async function processImage() {
+    step2(step1())
 
 }
 
 
 
 processImage();
-
+  
 
 cardNames=["2c","2d","2h","2s","3c","3d","3h","3s","4c","4d","4h","4s","5c","5d","5h","5s","6c","6d","6h","6s","7c","7d","7h","7s","8c","8d","8h","8s","9c","9d","9h","9s","10c","10d","10h","10s","ac","ad","ah","as","bj"]
