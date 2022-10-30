528e10614e5c9f724c02c5c13a584193abb2a2e5
anthonyng288 <anthonynguyen832@gmail.com>
2022-10-30 13:14:29 +1000

    refactor: clean up the structure of the card route

--- routes/card.js
+++ routes/card.js
@@ -2,7 +2,10 @@ const express = require('express');
 const https = require('https');
 const router = express.Router();
 
-const {processImage} = require('../cardGenerator.js')
+const { processImage } = require('../cardGenerator.js')
+
+cardNames=["2c","2d","2h","2s","3c","3d","3h","3s","4c","4d","4h","4s","5c","5d","5h","5s","6c","6d","6h","6s","7c","7d","7h","7s","8c","8d","8h","8s","9c","9d","9h","9s","10c","10d","10h","10s","ac","ad","ah","as","bj","jc","jd","jh","js","kc","kd","kh","ks","qc","qd","qh","qs"]
+
 
 router.get('/:query', (req, res) => {
     const theme = encodeURI(req.params.query); 
@@ -15,19 +18,20 @@ router.get('/:query', (req, res) => {
         })
         flickRes.on('end', function() {
             res.writeHead(flickRes.statusCode,{'content-type' :
-        'text/html'});
+            'text/html'});
 
-        const bodyString = body.join('')
-        const rsp = JSON.parse(bodyString)
-        const s = createPage('Flickr Photo Search', rsp)
-        res.write(s)
-        res.end()
+            const bodyString = body.join('')
+            const rsp = JSON.parse(bodyString)
+            const s = createPage('Flickr Photo Search', rsp)
+            res.write(s)
+            res.end()
         }) 
-        });
-        flickReq.on('error', (e) => {
-            console.error(e);
-        })
-        flickReq.end();
+    });
+
+    flickReq.on('error', (e) => {
+        console.error(e);
+    })
+    flickReq.end();
     
     
 })
@@ -58,16 +62,12 @@ function createFlickrOptions(query,number) {
     return options;
 } 
 
-cardNames=["2c","2d","2h","2s","3c","3d","3h","3s","4c","4d","4h","4s","5c","5d","5h","5s","6c","6d","6h","6s","7c","7d","7h","7s","8c","8d","8h","8s","9c","9d","9h","9s","10c","10d","10h","10s","ac","ad","ah","as","bj","jc","jd","jh","js","kc","kd","kh","ks","qc","qd","qh","qs"]
 
-//Various font sizes used to fit URL on screen
 function parsePhotoRsp(rsp) {
     for (let i = 0; i < rsp.photos.photo.length; i++) {
     photo = rsp.photos.photo[i];
     url = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_c.jpg`;
-    console.log(cardNames[i])
     processImage(url, cardNames[i])
-
     }
 }
 
