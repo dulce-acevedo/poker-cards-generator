c755136ece5500dc3e4632a7279adb7fc85e7aa5
dulce-acevedo <n10370021@qut.edu.au>
2022-11-07 18:11:31 +1000

    Added message in server

--- src/server/index.js
+++ src/server/index.js
@@ -11,6 +11,11 @@ const randomRouter = require('./routes/random');
 app.use(cors())
 app.use('/card', cardRouter);
 app.use('/random', randomRouter);
+app.get("/", (req, res) => {
+  res.send(
+    {serverMessage: "Welcome to the card-generator server"}
+  )
+})
 
 app.listen(port, () =>
   console.log('Express server listening on port 4000'),
